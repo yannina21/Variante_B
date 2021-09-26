d9fa1f8132d04ae6e083e3c8384110b72515b864
Victork1988 <valfonsomc@gmail.com>
2021-09-25 19:23:11 -0500

    guardarjs

--- form.js
+++ form.js
@@ -1,19 +1,94 @@
-var nombre = document.getElementById("usuario");
-var contraseña = document.getElementById("contraseña");
+function  checkUsername(valor){
+   
+  if( valor !=""){
+  if ((/^\w+([\.-]?)\w+/).test(valor)){
+     
+      if (valor.length<30){
+      return true;
+      }
+      else{
+          return false;
+      }
+  }
+  else{
+      return false;
+  }
+  
+}
+else{
+  return false;
+}
+}
 
-var form = document.getElementById("form-registro");
-form.addEventListener('submit', function(evt){
-    evt.preventDefault();
-    var mensajeerror = [];
-
-    if(usuario.value === null || usuario.value === ''){
-        mensajeerror.push('ingresa tu usuario');
+  function checkCorreo(valor){
+    if (valor!=""){
+        if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(valor))
+        {
+            return true;
+        }
+        else{
+           
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
     }
 
-    if(contraseña.value === null || contraseña.value === ''){
-        mensajeerror.push('ingresa tu contraseña');
-   }
+function checkContrasena(valor){
+  if(valor!=""){
+     
+      if (/^(?=.*\d)(?=.*[a-zñ]).*[A-ZÑ]/.test(valor)){
+          if (valor.length >=8){
+              return true;
+          }
+          else{
+            return false;
+          }
+         
+      }
+      else{
+          return false;
+      }
+          
+      
+  }
+  else{
+      return false;
+  }
+}
 
-    error.innerHTML = mensajeerror.join(', ');
+function checkConfirmContrasena(valor1,valor2){
     
-});
+  if(valor1!="" && valor2!=""){
+     
+      if ((/^(?=.*\d)(?=.*[a-zñ]).*[A-ZÑ]/.test(valor1)) && (/^(?=.*\d)(?=.*[a-zñ]).*[A-ZÑ]/.test(valor2))) {
+          if ((valor1.length >=8) && (valor2.length>=8)) {
+               if(valor1==valor2){
+                  return true;
+                   }
+               else {
+                return false;
+                  }
+          }
+          else{
+            return  false;
+          }
+         
+      }
+      else{
+          return false;
+      }
+          
+      
+  }
+  else{
+      return false;
+  }
+}
+
+module.exports.checkCorreo = checkCorreo;
+module.exports.checkUsername = checkUsername;
+module.exports.checkContrasena = checkContrasena;
+module.exports.checkConfirmContrasena = checkConfirmContrasena;
